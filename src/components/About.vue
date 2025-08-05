c0b51e0d1a25695888385c2d0e6cbbebb5375316
LuisGFurlan <furlanluisgustavo03@gmail.com>
2025-08-05 16:52:39 -0300

    fix : Card do perfil estava quebrando layout

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,41 +1,49 @@
 <template>
-  <section id="about" class="relative h-screen bg-primary flex items-center justify-center px-4 scroll-mt-28">
-  <div class="w-full h-full flex flex-col items-center justify-center md:flex-row md:justify-around">
-    
-    <!-- Foto -->
-    <div class="text-center md:text-left">
-      <div class="w-[200px] h-[200px] md:w-[400px] md:h-[400px] bg-indigo-500 rounded-full flex items-center justify-center mx-auto md:mx-0">
-        <img
-          :src="foto"
-          alt="Minha Foto"
-          class="w-[180px] h-[180px] md:w-[380px] md:h-[380px] rounded-full object-cover border-4 border-white"
-        />
+  <section
+    id="about"
+    class="relative min-h-screen bg-primary flex items-center justify-center px-4 scroll-mt-28"
+  >
+    <!-- Container Geral -->
+    <div class="w-full h-full flex flex-col items-center justify-center md:flex-row md:justify-around">
+
+      <!-- Foto, Nome e Profissão -->
+      <div class="text-center md:text-center flex flex-col items-center">
+        <div
+          class="w-[200px] h-[200px] md:w-[400px] md:h-[400px] bg-indigo-500 rounded-full flex items-center justify-center"
+        >
+          <img
+            :src="foto"
+            alt="Minha Foto"
+            class="w-[180px] h-[180px] md:w-[380px] md:h-[380px] rounded-full object-cover border-4 border-white"
+          />
+        </div>
+        <h2 class="mt-2 text-2xl md:text-3xl font-bold text-white">
+          dev<span class="text-indigo-500">Furlan</span>
+        </h2>
+        <p class="text-xl text-indigo-600 font-medium mt-1">
+          Desenvolvedor Mobile
+        </p>
       </div>
-      <h2 class="mt-2 text-2xl md:text-3xl font-bold text-white">
-        dev<span class="text-indigo-500">Furlan</span>
-      </h2>
-      <p class="text-xl text-indigo-600 font-medium mt-1 hidden md:block">
-        Desenvolvedor Mobile
-      </p>
-    </div>
 
-    <!-- Texto -->
-    <div class="mt-8 md:mt-0 md:w-[600px] text-center md:text-left">
-      <h2 class="hidden md:block text-4xl font-bold text-indigo-600 w-full">
-        Sobre mim:
-      </h2>
-      <p class="text-xl md:text-2xl text-white font-medium mt-6">
-        <span class="md:hidden">Prazer!</span> Sou
-        <span class="text-indigo-500 md:text-indigo-600 font-semibold">Luis Gustavo Sardo Furlan</span>,
-        eterno estudante e <span class="text-indigo-500 md:text-indigo-600 font-semibold">Desenvolvedor mobile</span>
-        em atuação. A paixão pela programação sempre fez parte da minha vida e, com a experiência prática no mercado,
-        só aumenta espontaneamente <span class="text-indigo-500 md:text-indigo-600 font-semibold">:)</span>
-      </p>
-      <p class="text-xl text-gray-500 mt-8">Em busca do progresso</p>
-    </div>
-  </div>
-</section>
+      <!-- Texto "Sobre mim" -->
+      <div class="mt-8 md:mt-0 md:w-[600px] text-center md:text-left">
+        <h2 class="hidden md:block text-4xl font-bold text-indigo-600 w-full">
+          Sobre mim:
+        </h2>
+        <p class="text-xl md:text-2xl text-white font-medium mt-6">
+          <span class="md:hidden">Prazer!</span> Sou
+          <span class="text-indigo-500 md:text-indigo-600 font-semibold">Luis Gustavo Sardo Furlan</span>,
+          eterno estudante e
+          <span class="text-indigo-500 md:text-indigo-600 font-semibold">Desenvolvedor mobile</span> em atuação.
+          A paixão pela programação sempre fez parte da minha vida e, com a experiência prática no mercado,
+          só aumenta espontaneamente
+          <span class="text-indigo-500 md:text-indigo-600 font-semibold">:)</span>
+        </p>
+        <p class="text-xl text-gray-500 mt-8">Em busca do progresso</p>
+      </div>
 
+    </div>
+  </section>
 </template>
 
 <script setup>
